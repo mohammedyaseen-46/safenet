c0cf3ea74fbb2c3a38635c721347fc4e0dddd943
Mohammed Yaaseen S <smohammedyaseen46@gmail.com>
2026-07-26 17:19:54 +0530

    bugs fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
